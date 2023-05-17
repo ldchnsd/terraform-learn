bb46e6b728f32697687c2e5ae8dcdf7554bde968
ldchnsd <youqingtian99520@gmail.com>
2023-05-17 10:56:55 +0000

    use vpc module form tf registry

--- modules/webserver/main.tf
+++ modules/webserver/main.tf
@@ -1,4 +1,4 @@
-resource "aws_default_security_group" "default-sg" {
+resource "aws_security_group" "myapp-sg" {
   vpc_id = var.vpc_id
 
   ingress {
@@ -50,7 +50,7 @@ resource "aws_instance" "myapp-server" {
   instance_type = var.instance_type
 
   subnet_id = var.subnet_id
-  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+  vpc_security_group_ids = [aws_security_group.myapp-sg.id]
   availability_zone = var.avail_zone
   associate_public_ip_address = true
 
